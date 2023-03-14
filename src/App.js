04504c131d2a2c524d77ffb90db8c5a22670c785
Uttu32 <utkarshksharm7878@gmail.com>
2023-03-15 00:15:07 +0530

    React-Day-6-useRef-and-useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState } from "react";
+import "./App.css";
+import { MdCancel } from "react-icons/md";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const initialValue = useRef(0);
+
+  const handelChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handelAdding = () => {
+    const newUser = {
+      id: new Date() + text,
+      title: text,
+      status: false,
+    };
+    setTodos([...todos, newUser]);
+    setText("");
+  };
+
+  const handelDelete = (id) => {
+    const filterData=todos.filter((todo) => todo.id!==id)
+    setTodos(filterData);
+  };
+
+  const handelStatus = (id) => {
+    const updateData = todos.map((item) => {
+      if (item.id === id) {
+        initialValue.current = initialValue.current + 1;
+        return { ...item, status: !item.status };
+      } else {
+        return item;
+      }
+    });
+    setTodos(updateData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pending Task {todos.length - initialValue.current}</h1>
+      <div>
+        {todos &&
+          todos.map((ele, index) => (
+            <div key={index} className="Single-data">
+              {ele.status ? (
+                <p>
+                  {" "}
+                  <strike> {ele.title}</strike>
+                </p>
+              ) : (
+                <p>{ele.title}</p>
+              )}
+              <div>
+                <button onClick={() => handelStatus(ele.id)}>Completed</button>
+                <MdCancel
+                  className="icon"
+                  onClick={() => handelDelete(ele.id)}
+                />
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="input-part">
+        <input type="text" value={text} onChange={(e) => handelChange(e)}/>
+        <button onClick={handelAdding}>Add</button>
+        
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
+
+
+
+
